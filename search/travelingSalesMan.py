bd7b287eab55e0f8fee155d22b170ea6aa3185a2
B1naryB0t <47212121+MahdiTheGreat@users.noreply.github.com>
2023-09-17 16:54:18 +0330

    Delete search directory

--- search/travelingSalesMan.py
+++
@@ -1,9 +0,0 @@
-
-corners=[(1, 1), (1, 8), (1, 16), (1, 17)]
-currentPosition=(1,6)
-locations=[]
-for i in range(len(corners)):
-    locations.append(corners[i])
-locations.append(currentPosition)
-
-
